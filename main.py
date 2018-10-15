3db92d0f9f1a2eed1b38c28fb64b11bce949890d
Manuel <manuelkaiser92@gmail.com>
2018-10-15 09:38:25 +0200

    labeling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import labeling
+import labeling
 # import time
 import my_model
 import my_generator
@@ -9,16 +9,16 @@ directory = "test_data/"
 
 # print("Labeling data...")
 # start = time.clock()
-# data, metadata = labeling.get_data(dir + "data/0810/hc/data.dat")
-# scenes_labeled, scenarios_labeled = labeling.label_scenarios(data, metadata)
-# labeling.save_video(dir + "data/0810/hc/frames/", dir + "data/0810/hc/video.avi", scenes_labeled, scenarios_labeled)
+data, metadata = labeling.get_data(dir + "data/0810/hc/data.dat")
+scenes_labeled, scenarios_labeled = labeling.label_scenarios(data, metadata)
+labeling.save_video(dir + "data/0810/hc/frames/", dir + "data/0810/hc/video.avi", scenes_labeled, scenarios_labeled)
 # diff = time.clock() - start
 # print("Labeling data took " + str(round(diff, 2)) + " seconds")
 
 
 """ Build, compile and train model """
-model = my_model.build_model()
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-train_generator, validation_generator = my_generator.build_data_generators(directory)
-model.fit_generator(generator=train_generator, validation_data=validation_generator, epochs=5)
+# model = my_model.build_model()
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# print(model.summary())
+# train_generator, validation_generator = my_generator.build_data_generators(directory)
+# model.fit_generator(generator=train_generator, validation_data=validation_generator, epochs=5)
