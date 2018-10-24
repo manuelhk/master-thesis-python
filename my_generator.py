32cb8ecffd25df6f1cb721d58f255e42385d73f7
Manuel <manuelkaiser92@gmail.com>
2018-10-24 11:07:48 +0200

    moving import fcs to my_generator.py

--- my_generator.py
+++ my_generator.py
@@ -1,11 +1,12 @@
+import glob
+import random
 import keras
-import preprocessing
 import numpy as np
 
 
 """ 
-    Article: A detailed example of how to use data generators with Keras
     Source: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly 
+            A detailed example of how to use data generators with Keras
 """
 
 
@@ -67,7 +68,31 @@ def build_data_generators(directory):
               'n_classes': 2,
               'n_channels': 3,
               'shuffle': True}
-    data, labels = preprocessing.get_data_and_labels(directory)
+    data, labels = get_data_and_labels(directory)
     train_generator = DataGenerator(data['train'], labels, **params)
     validation_generator = DataGenerator(data['validation'], labels, **params)
     return train_generator, validation_generator
+
+
+def get_data_and_labels(directory):
+    """ Accesses directory and creates two dictionaries with filenames .npy. One for labels and the other for data. """
+    paths = glob.glob(directory + "/*/*.npy")
+    # paths.sort()
+    labels_dict = dict()
+    for path in paths:  # todo create method to label data correctly
+        if "label0" in path:
+            labels_dict.update({path: 0})
+        else:
+            labels_dict.update({path: 1})
+    # labels = [0 if "left" in path else 1 for path in paths]
+    data_dict = split_into_training_and_validation(paths)
+    return data_dict, labels_dict
+
+
+def split_into_training_and_validation(list_of_paths, share_training_data=0.8):
+    """ Splits data into training data and validation data and returns a dictionary with two lists """
+    random.shuffle(list_of_paths)
+    train_list = list_of_paths[:np.math.floor(len(list_of_paths)*share_training_data)]
+    eval_list = list_of_paths[np.math.floor(len(list_of_paths)*share_training_data):]
+    data_dict = {"train": train_list, "validation": eval_list}
+    return data_dict
--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import glob
-import random
 
 
 IMAGE_SHAPE = (150, 150)
@@ -47,27 +46,3 @@ def images_to_npy(directory):
         np.save(directory + str(i) + ".npy", load_image(path))
         i += 1
     pass
-
-
-def get_data_and_labels(directory):
-    """ Accesses directory and creates two dictionaries with filenames .npy. One for labels and the other for data. """
-    paths = glob.glob(directory + "/*/*.npy")
-    # paths.sort()
-    labels_dict = dict()
-    for path in paths:  # todo create method to label data correctly
-        if "label0" in path:
-            labels_dict.update({path: 0})
-        else:
-            labels_dict.update({path: 1})
-    # labels = [0 if "left" in path else 1 for path in paths]
-    data_dict = split_into_training_and_validation(paths)
-    return data_dict, labels_dict
-
-
-def split_into_training_and_validation(list_of_paths, share_training_data=0.8):
-    """ Splits data into training data and validation data and returns a dictionary with two lists """
-    random.shuffle(list_of_paths)
-    train_list = list_of_paths[:np.math.floor(len(list_of_paths)*share_training_data)]
-    eval_list = list_of_paths[np.math.floor(len(list_of_paths)*share_training_data):]
-    data_dict = {"train": train_list, "validation": eval_list}
-    return data_dict
