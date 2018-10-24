6b5b3f39ab9847f0bad44750a708fd5db5d17b42
Manuel <manuelkaiser92@gmail.com>
2018-10-24 14:30:02 +0200

    Update preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import glob
+
+
+def load_video(video_path):
+    """ Loads images in defined folder (from one video) and returns numpy of video """
+    images = glob.glob(video_path + "/*.jpg")
+    images.sort()
+    img_list = []
+    for image_path in images:
+        image = load_image(image_path)
+        image = cv2.resize(image, (150, 150))
+        img_list.append(image)
+    video_np = np.array(img_list)
+    return video_np
+
+
+def load_image(image_path):
+    """ Loads image and returns numpy of image"""
+    image = cv2.imread(image_path)
+    image = cv2.resize(image, (150, 150))
+    image_np = np.array(image)
+    return image_np
+
+
+def videos_to_npy(directory):
+    """ Accesses every video folder in directory and saves a .npy file respectively"""
+    videos = glob.glob(directory + "*")
+    for i, path in enumerate(videos):
+        np.save(directory + str(i) + ".npy", load_video(path))
+    pass
+
+
+def images_to_npy(directory):
+    """ Accesses every image in directory and creates and saves a .npy file respectively """
+    images = glob.glob(directory + "*.jpg")
+    images.sort()
+    for i, path in enumerate(images):
+        np.save(directory + str(i) + ".npy", load_image(path))
+    pass
