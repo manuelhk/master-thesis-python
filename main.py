3a8056d0b092f2a7894ea133aea6015aaaedcd0c
Manuel <manuelkaiser92@gmail.com>
2018-10-24 11:03:01 +0200

    split main

--- main.py
+++ main.py
@@ -1,19 +1,1 @@
 
-
-
-
-
-
-
-""" Label and save data as *.npy-files """
-# print("Labeling data...")
-# start = time.clock()
-# data, metadata = labeling.get_data("data/data/0810/hc/data.dat")
-# scenes_labeled, scenarios_labeled = labeling.label_scenarios(data, metadata)
-# labeling.save_video(dir + "data/0810/hc/frames/", dir + "data/0810/hc/video.avi", scenes_labeled, scenarios_labeled)
-# diff = time.clock() - start
-# print("Labeling data took " + str(round(diff, 2)) + " seconds")
-
-
-""" Build, compile and train model """
-
---
+++ main_preparation.py
@@ -0,0 +1,9 @@
+import labeling_basic as lb
+
+
+""" Labeling data..."""
+data, metadata, all_vehicles, images = lb.get_data("data/data/0808/data.dat", "data/data/0808/frames")
+label_dict, scenarios_dict = lb.label_scenarios(data, metadata, all_vehicles, images)
+lb.save_video(label_dict, "data/data/0808/video_new.avi")
+
+
