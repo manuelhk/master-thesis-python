39faa473e730369f377d0bdadc7b58a5206fa1c1
Manuel <manuelkaiser92@gmail.com>
2018-11-02 08:52:05 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -3,41 +3,99 @@ import numpy as np
 import math
 
 
+X = np.arange(-5., 5., 0.01)
+Y_LABEL = "$\\varphi(x)$"
+X_LABEL = "x"
+FONT_SIZE = 16
+GRID = True
+
+
+
 def plot_sigmoid():
-    x = np.arange(-5., 5., 0.1)
     sig = []
-    for item in x:
+    for item in X:
         sig.append(1 / (1 + math.exp(-item)))
 
-    plt.plot(x, sig)
+    fig = plt.figure()
+    plt.plot(X, sig)
 
-    plt.ylabel("$\\varphi(x)$")
-    plt.xlabel("x")
-    plt.grid(True)
-    plt.ylim(bottom=0, top=1)
+    plt.ylabel(Y_LABEL, fontsize=FONT_SIZE)
+    plt.xlabel(X_LABEL, fontsize=FONT_SIZE)
+    plt.grid(GRID)
+
+    plt.ylim(bottom=-0.1, top=1.1)
     plt.xlim(left=-5, right=5)
 
+    fig.savefig("act_sigmoid.png")
+    plt.show()
+    pass
+
+
+
+
+def plot_tanh():
+    tanh = np.tanh(X)
+
+    fig = plt.figure()
+    plt.plot(X, tanh)
+
+    plt.ylabel(Y_LABEL, fontsize=FONT_SIZE)
+    plt.xlabel(X_LABEL, fontsize=FONT_SIZE)
+    plt.grid(GRID)
+
+    plt.ylim(bottom=-1.2, top=1.2)
+    plt.xlim(left=-5, right=5)
+
+    fig.savefig("act_tanh.png")
+    plt.show()
+    pass
+
+
+def plot_step_fc():
+    step = []
+    for item in X:
+        if item < 0:
+            step.append(0)
+        else:
+            step.append(1)
+
+    fig = plt.figure()
+    plt.plot(X, step)
+
+    plt.ylabel(Y_LABEL, fontsize=FONT_SIZE)
+    plt.xlabel(X_LABEL, fontsize=FONT_SIZE)
+    plt.grid(GRID)
+
+    plt.ylim(bottom=-0.1, top=1.1)
+    plt.xlim(left=-5, right=5)
+
+    fig.savefig("act_step_fc.png")
     plt.show()
     pass
 
 
 def plot_relu():
-    x = np.arange(-5., 5., 0.1)
     relu = []
-    for item in x:
+    for item in X:
         relu.append(max(0, item))
 
-    plt.plot(x, relu)
+    fig = plt.figure()
+    plt.plot(X, relu)
 
-    plt.ylabel("$\\varphi(x)$")
-    plt.xlabel("x")
-    plt.grid(True)
-    plt.ylim(bottom=-1, top=5)
+    plt.ylabel(Y_LABEL, fontsize=FONT_SIZE)
+    plt.xlabel(X_LABEL, fontsize=FONT_SIZE)
+    plt.grid(GRID)
+
+    plt.ylim(bottom=-0.5, top=5.5)
     plt.xlim(left=-5, right=5)
 
+    fig.savefig("act_relu.png")
     plt.show()
     pass
 
 
-plot_sigmoid()
-plot_relu()
+#plot_sigmoid()
+#plot_step_fc()
+#plot_tanh()
+#plot_relu()
+
