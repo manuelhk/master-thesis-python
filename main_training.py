33664b33ed41f48929901a56f4218c4e7f164f97
manuelhk <manuel@mhkaiser.de>
2018-11-06 18:04:47 +0100

    working neural network

--- main_training.py
+++ main_training.py
@@ -1,13 +1,12 @@
 import my_model
 import my_generator
 import keras
-import time
 
 
-DIRECTORY = "test"
+DIRECTORY = "training"
 SCENARIOS = ["free_cruising", "following", "catching_up"]
-PARAMS = {'dim': (15, 150, 150),
-          'batch_size': 20,
+PARAMS = {'dim': (15, 299, 299),
+          'batch_size': 2,
           'n_classes': SCENARIOS.__len__(),
           'n_channels': 3,
           'shuffle': True}
@@ -15,11 +14,11 @@ PARAMS = {'dim': (15, 150, 150),
 
 def training():
     model = my_model.build_model_inceptionV3_LSTM(SCENARIOS.__len__())
-    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(lr=0.1), metrics=["accuracy"])
+    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(3e-4), metrics=["accuracy"])
     print(model.summary())
     train_generator, validation_generator = my_generator.build_data_generators(DIRECTORY, SCENARIOS, PARAMS)
-    model.fit_generator(generator=train_generator, validation_data=validation_generator, epochs=2)
-    model.save("test/my_model_v3_lstm.h5")
+    history = model.fit_generator(generator=train_generator, validation_data=validation_generator, epochs=2)
+    model.save(DIRECTORY + "/my_model_v3_lstm.h5")
     return model
 
 
