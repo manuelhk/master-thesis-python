0e73ad29a88aa76e808f8638d5e0372017a205a4
Manuel <manuelkaiser92@gmail.com>
2018-11-08 17:01:18 +0100

    visualization for training history

--- helper.py
+++ helper.py
@@ -1,6 +1,9 @@
 import numpy as np
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import cv2
+import itertools
+
 
 def video_to_jpges_and_npys(video_path, output_path):
     vidcap = cv2.VideoCapture(video_path)
@@ -37,7 +40,63 @@ def show_npy(path, number_of_images=3, title="No Title"):
     pass
 
 
+def show_training_history(history_path):
+    print("Load history...")
+    history_np = np.load(history_path)
+    # plot the model's accuracy
+    fig = plt.figure()
+    plt.plot(history_np.item().history.get("acc"))
+    plt.plot(history_np.item().history.get("val_acc"))
+    plt.title("Model accuracy")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Epoch")
+    plt.legend(["Train", "Validation"])
+    plt.xticks(np.arange(20, dtype="int"), np.arange(20, dtype="int") + 1)
+    plt.ylim(bottom=0, top=1)
+    plt.xlim(left=0, right=19)
+    plt.show()
+    fig.savefig("output/accuracy.png")
+    # plot the model's loss
+    fig = plt.figure()
+    plt.plot(history_np.item().history.get("loss"))
+    plt.plot(history_np.item().history.get("val_loss"))
+    plt.title("Model loss")
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.xticks(np.arange(20, dtype="int"), np.arange(20, dtype="int") + 1)
+    plt.ylim(bottom=0, top=1.7)
+    plt.xlim(left=0, right=19)
+    plt.legend(["Train", "Validation"])
+    plt.show()
+    fig.savefig("output/loss.png")
+    pass
+
+
+def show_confusion_matrix(y_true, y_pred, label_names, title="Confusion matrix"):
+    cm = confusion_matrix(y_true, y_pred)
+    fig = plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(label_names))
+    plt.xticks(tick_marks, label_names, rotation=45)
+    plt.yticks(tick_marks, label_names)
+    plt.tight_layout()
+    fmt = "d"
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+    fig.savefig("output/confusion_matrix.png")
+    pass
+
 # l = glob.glob("test/*/*.npy")
 # show_npy('test/FREE_CRUISING/FREE_CRUISING_9.npy', 15)
 
 # video_to_jpges_and_npys("data/video.avi", "data/video/")
+
+# show_training_history("/Users/manuel/Dropbox/_data/_models/1108_v3_lstm_fr_fo_ca_lcl_lcr_950_20/history.npy")
